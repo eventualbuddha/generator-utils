3643049dec16868a6ade1b1f4a2b6210210f00e1
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-07-30 15:26:05 -0700

    feat: add `copy` to make replayable iterables
    Also switches from Mocha to Jest and adds Eslint.

---
+++ src/__tests__/test.ts
@@ -0,0 +1,200 @@
+import {
+  combine,
+  concat,
+  copy,
+  filter,
+  filterMap,
+  flatten,
+  forEach,
+  fromArray,
+  map,
+  range,
+  take,
+  toArray
+} from '..';
+
+describe('generator-utils', () => {
+  describe('combine', () => {
+    it('returns an empty generator when given an empty list', () => {
+      expect(Array.from(combine([]))).toStrictEqual([]);
+    });
+
+    it('returns a new generator yielding values wrapped in arrays when there is only one generator', () => {
+      const nums = combine([range(0, 3)]);
+      expect(Array.from(nums)).toStrictEqual([[0], [1], [2], [3]]);
+    });
+
+    it('returns a new generator yielding pairs of all values when given two generators', () => {
+      const pairs = combine([range(0, 1), range(3, 4)]);
+      expect(Array.from(pairs)).toStrictEqual([[0, 3], [0, 4], [1, 3], [1, 4]]);
+    });
+
+    it('returns a new generator yielding tuples of all values when given many generators', () => {
+      const tuples = combine([range(0, 1), range(3, 4), range(6, 7)]);
+      expect(Array.from(tuples)).toStrictEqual([
+        [0, 3, 6],
+        [0, 3, 7],
+        [0, 4, 6],
+        [0, 4, 7],
+        [1, 3, 6],
+        [1, 3, 7],
+        [1, 4, 6],
+        [1, 4, 7]
+      ]);
+    });
+
+    it('works with infinite generators', () => {
+      const tuples = combine([naturalNumbers(), naturalNumbers()]);
+
+      let i = 0;
+      for (const tuple of tuples) {
+        expect(tuple).toStrictEqual([0, i++]);
+
+        if (i > 10000) {
+          break;
+        }
+      }
+    });
+  });
+
+  describe('concat', () => {
+    it('returns an empty generator when given an empty list', () => {
+      expect(Array.from(concat([]))).toStrictEqual([]);
+    });
+
+    it('yields values from the first generator in the list if there is only one', () => {
+      const nums = concat([naturalNumbers()]);
+      expect(take(nums, 3)).toStrictEqual([0, 1, 2]);
+    });
+
+    it('returns a new generator that yields the values from the first generator, second, etc', () => {
+      const g = concat([range(0, 1), range(3, 4), range(6, 7)]);
+      expect(Array.from(g)).toStrictEqual([0, 1, 3, 4, 6, 7]);
+    });
+  });
+
+  describe('copy', () => {
+    it('works with empty generators', () => {
+      const copied = copy(fromArray([]));
+      expect(Array.from(copied)).toStrictEqual([]);
+    });
+
+    it('works with infinite generators', () => {
+      const copied = copy(naturalNumbers());
+      expect(take(copied, 3)).toStrictEqual([0, 1, 2]);
+    });
+  });
+
+  describe('filter', () => {
+    it('allows skipping values', () => {
+      const evens = filter(naturalNumbers(), function(n) {
+        return n % 2 === 0;
+      });
+
+      expect(take(evens, 3)).toStrictEqual([0, 2, 4]);
+    });
+  });
+
+  describe('filterMap', () => {
+    it('allows skipping values and mapping at the same time', () => {
+      const negativeOdds = filterMap(naturalNumbers(), function(n, skip) {
+        if (n % 2 === 0) {
+          skip();
+        } else {
+          return -n;
+        }
+      });
+
+      expect(take(negativeOdds, 3)).toStrictEqual([-1, -3, -5]);
+    });
+  });
+
+  describe('forEach', () => {
+    it('calls the given iterator function with each generator value', () => {
+      let values: Array<number> = [];
+
+      forEach(range(2, 5), value => {
+        values.push(value);
+      });
+
+      expect(values).toStrictEqual([2, 3, 4, 5]);
+    });
+  });
+
+  describe('fromArray', () => {
+    it('returns an empty generator from an empty array', () => {
+      const nothing = fromArray([]);
+      expect(Array.from(nothing)).toStrictEqual([]);
+    });
+
+    it('returns a generator with all the elements from the array in the same order', () => {
+      const nums = [0, 4, 7, 9];
+      const g = fromArray(nums);
+
+      expect(Array.from(g)).toStrictEqual([0, 4, 7, 9]);
+    });
+  });
+
+  describe('map', () => {
+    it('allows changing each value of a generator into another value', () => {
+      const doubleNaturalNumbers = map(naturalNumbers(), function(n) {
+        return n * 2;
+      });
+
+      expect(take(doubleNaturalNumbers, 3)).toStrictEqual([0, 2, 4]);
+    });
+  });
+
+  describe('flatten', () => {
+    it('allows unwrapping generators to values', () => {
+      const gens = [range(0, 0), range(2, 4)];
+      const numbers = flatten(fromArray(gens));
+
+      expect(Array.from(numbers)).toStrictEqual([0, 2, 3, 4]);
+    });
+  });
+
+  describe('range', () => {
+    it('returns a generator from N to M', () => {
+      const zeroThroughThree = range(0, 3);
+
+      expect(Array.from(zeroThroughThree)).toStrictEqual([0, 1, 2, 3]);
+    });
+
+    it('returns a generator yielding nothing if N > M', () => {
+      const nothing = range(0, -1);
+      expect(Array.from(nothing)).toStrictEqual([]);
+    });
+
+    it('returns a generator yielding only N if N == M', () => {
+      const zero = range(0, 0);
+      expect(Array.from(zero)).toStrictEqual([0]);
+    });
+  });
+
+  describe('take', () => {
+    it('returns the first N values from a generator', () => {
+      const firstTen = take(naturalNumbers(), 10);
+      expect(firstTen).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    it('returns all the values of a generator if it has less than N values', () => {
+      const zeroThroughThree = range(0, 3);
+      expect(take(zeroThroughThree, 5)).toStrictEqual([0, 1, 2, 3]);
+    });
+  });
+
+  describe('toArray', () => {
+    it('returns an array by consuming all of the available values in a generator', () => {
+      const zeroThroughThree = range(0, 3);
+      expect(toArray(zeroThroughThree)).toStrictEqual([0, 1, 2, 3]);
+    });
+  });
+
+  function* naturalNumbers(): Iterable<number> {
+    let value = 0;
+    while (true) {
+      yield value++;
+    }
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,66 @@
 /**
- * Creates a new generator yielding all in-order combinations of values from
- * the given generators. All values from all generators will be read, so do not
- * use this with infinite generators.
+ * Creates a new iterable yielding all in-order combinations of values from
+ * the given iterables.
  *
  * @example
  *
  *   toArray(combine([range(0, 1), range(4, 5)]))
  *   // [[0, 4], [0, 5], [1, 4], [1, 5]]
  */
-export function *combine<T>(generators: Array<IterableIterator<T>>): IterableIterator<Array<T>> {
-  switch (generators.length) {
+export function* combine<T>(iterables: Array<Iterable<T>>): Iterable<Array<T>> {
+  switch (iterables.length) {
     case 0:
       return;
 
     case 1:
-      for (let value of generators[0]) {
+      for (const value of iterables[0]) {
         yield [value];
       }
       break;
 
-    default:
-      let [head, ...tail] = generators;
-      let headValues = Array.from(head);
-      let tailCombinations = Array.from(combine(tail));
+    default: {
+      const tails = copy(combine(iterables.slice(1)));
 
-      for (let headValue of headValues) {
-        for (let tailCombination of tailCombinations) {
-          yield [headValue, ...tailCombination];
+      for (const head of iterables[0]) {
+        for (const tail of tails) {
+          yield [head, ...tail];
         }
       }
       break;
+    }
   }
 }
 
 /**
- * Returns a generator yielding all the values from the given generators in
- * order. If any generators are infinite generators none of the values from
- * subsequent generators will be read.
+ * Returns an iterable yielding all the values from the given iterables in
+ * order. If any iterables are infinite then none of the values from
+ * subsequent iterables will be read.
  *
  * @example
  *
  *   toArray(concat([range(0, 1), range(4, 5)]))
  *   // [0, 1, 4, 5]
  */
-export function *concat<T>(generators: Array<IterableIterator<T>>): IterableIterator<T> {
-  for (let generator of generators) {
-    yield *generator;
+export function* concat<T>(iterables: Array<Iterable<T>>): Iterable<T> {
+  for (const iterable of iterables) {
+    yield* iterable;
   }
 }
 
 /**
- * Returns a generator that yields values from another generator passing a
- * predicate. This may be used with infinite generators, though care should be
- * taken to ensure that the predicate does not return false for all values.
+ * Returns an iterable that yields values from another iterable passing a
+ * predicate.
  *
  * @example
  *
  *   toArray(filter(range(0, 5), x => x % 2 === 0))
  *   // [0, 2, 4]
  */
-export function *filter<T>(generator: IterableIterator<T>, predicate: (value: T) => boolean) {
-  for (let value of generator) {
+export function* filter<T>(
+  iterable: Iterable<T>,
+  predicate: (value: T) => boolean
+): Iterable<T> {
+  for (const value of iterable) {
     if (predicate(value)) {
       yield value;
     }
@@ -77,12 +77,15 @@ export function *filter<T>(generator: IterableIterator<T>, predicate: (value: T)
  *   toArray(filter(range(0, 5), (x, skip) => (x % 2 === 0) ? skip() : x * x))
  *   // [1, 9, 25]
  */
-export function *filterMap<T, U>(generator: IterableIterator<T>, transform: (value: T, skip: () => void) => U | undefined): Iterator<U> {
-  for (let value of generator) {
+export function* filterMap<T, U>(
+  iterable: Iterable<T>,
+  transform: (value: T, skip: () => void) => U | undefined
+): Iterable<U> {
+  for (const value of iterable) {
     let skipped = false;
-    function skip() { skipped = true; }
-
-    let mappedValue = transform(value, skip);
+    const mappedValue = transform(value, () => {
+      skipped = true;
+    });
 
     if (!skipped && mappedValue !== undefined) {
       yield mappedValue;
@@ -91,88 +94,95 @@ export function *filterMap<T, U>(generator: IterableIterator<T>, transform: (val
 }
 
 /**
- * Makes a generator from a generator producing other generators.
+ * Makes an iterable from an iterable producing other iterables.
  *
  * @example
  *
  *   toArray(flatten(fromArray([range(2, 5), range(6,7))))
  *   // [2,3,4,5,6,7]
  */
-export function *flatten<T>(generator: IterableIterator<IterableIterator<T>>): IterableIterator<T> {
-  for (let yieldedGenerator of generator) {
-    yield *yieldedGenerator;
+export function* flatten<T>(iterable: Iterable<Iterable<T>>): Iterable<T> {
+  for (const yieldedIterable of iterable) {
+    yield* yieldedIterable;
   }
 }
 
 /**
- * Calls a function for each value in a generator.
+ * Calls a function for each value in an iterable.
  *
  * @example
  *
  *   forEach(range(1, 4), console.log)
  *   // prints "1\n2\n3\n4\n"
  */
-export function forEach<T>(generator: IterableIterator<T>, iterator: (value: T) => void) {
-  for (let value of generator) {
-    iterator(value);
+export function forEach<T>(
+  iterable: Iterable<T>,
+  callback: (value: T) => void
+): void {
+  for (const value of iterable) {
+    callback(value);
   }
 }
 
 /**
- * Returns a generator yielding the values from the given array in order.
+ * Returns an iterable yielding the values from the given array in order.
  *
  * @example
  *
  *   toArray(fromArray([1, 2, 3]))
  *   // [1, 2, 3]
  */
-export function *fromArray<T>(array: Array<T>): IterableIterator<T> {
-  for (let value of array) {
+export function* fromArray<T>(array: Array<T>): Iterable<T> {
+  for (const value of array) {
     yield value;
   }
 }
 
 /**
- * Maps one generator to another by passing all values through a transformer.
+ * Maps one iterable to another by passing all values through a transformer.
  *
  * @example
  *
  *   toArray(map(range(2, 5), x => x * 2))
  *   // [4, 6, 8, 10]
  */
-export function *map<T, U>(generator: IterableIterator<T>, transformer: (value: T) => U): IterableIterator<U> {
-  for (let value of generator) {
+export function* map<T, U>(
+  iterable: Iterable<T>,
+  transformer: (value: T) => U
+): Iterable<U> {
+  for (const value of iterable) {
     yield transformer(value);
   }
 }
 
 /**
- * Returns a generator yielding values from min up to and including max.
+ * Returns an iterable yielding values from min up to and including max.
  *
  * @example
  *
  *   toArray(range(8, 10))
  *   // [8, 9, 10]
  */
-export function *range(min: number, max: number): IterableIterator<number> {
+export function* range(min: number, max: number): Iterable<number> {
   for (let i = min; i <= max; i++) {
     yield i;
   }
 }
 
 /**
- * Returns up to the first count values of a generator.
+ * Returns up to the first count values of an iterable.
  *
  * @example
  *
  *   take(range(10, 20), 3)
  *   // [10, 11, 12]
  */
-export function take<T>(generator: IterableIterator<T>, count: number): Array<T> {
-  let result = [];
+export function take<T>(iterable: Iterable<T>, count: number): Array<T> {
+  const result = [];
+  const iterator = iterable[Symbol.iterator]();
 
   while (count-- > 0) {
-    let iteration = generator.next();
+    const iteration = iterator.next();
 
     if (iteration.done) {
       if (iteration.value !== undefined) {
@@ -188,8 +198,8 @@ export function take<T>(generator: IterableIterator<T>, count: number): Array<T>
 }
 
 /**
- * Reads all values from a generator and returns an array containing them. Be
- * careful not to use this with infinite generators, as it will never return and
+ * Reads all values from an iterable and returns an array containing them. Be
+ * careful not to use this with infinite iterables, as it will never return and
  * your program will eventually run out of memory.
  *
  * @example
@@ -197,6 +207,75 @@ export function take<T>(generator: IterableIterator<T>, count: number): Array<T>
  *   toArray(range(7, 9))
  *   // [7, 8, 9]
  */
-export function toArray<T>(generator: IterableIterator<T>): Array<T> {
-  return Array.from(generator);
+export function toArray<T>(iterable: Iterable<T>): Array<T> {
+  return Array.from(iterable);
+}
+
+/**
+ * Create a new iterable that, whenever it is iterated over, yields the same
+ * values from `iterable` in order.
+ *
+ * @example
+ *
+ *   const copied = copy(range(0, 2));
+ *   const notCopied = range(0, 2);
+ *
+ *   // Consumes the underlying iterable as-is…
+ *   toArray(copied);
+ *   // [0, 1, 2]
+ *
+ *   // …but caches the results to be replayed.
+ *   toArray(copied);
+ *   // [0, 1, 2]
+ *
+ *   // Consumes the iterable…
+ *   toArray(notCopied);
+ *   // [0, 1, 2]
+ *
+ *   // …and has nothing left.
+ *   toArray(notCopied);
+ *   // []
+ */
+export function copy<T>(iterable: Iterable<T>): Iterable<T> {
+  const values: Array<T> = [];
+  const indexes: Array<number> = [];
+  const iterators: Array<Iterator<T>> = [];
+  let done = false;
+  let maxIndex = Infinity;
+
+  function getIteratorResultAtIndex(index: number): IteratorResult<T> {
+    while (values.length <= index) {
+      if (!done) {
+        const valueArray = take(iterable, 1);
+
+        if (valueArray.length === 0) {
+          done = true;
+          maxIndex = values.length - 1;
+        }
+
+        values.push(valueArray[0]);
+      }
+    }
+
+    return {
+      done: index > maxIndex,
+      value: values[index]
+    };
+  }
+
+  return {
+    [Symbol.iterator](): Iterator<T> {
+      const id = iterators.length;
+
+      indexes[id] = 0;
+      iterators[id] = {
+        next(): IteratorResult<T> {
+          const result = getIteratorResultAtIndex(indexes[id]++);
+          return result;
+        }
+      };
+
+      return iterators[id];
+    }
+  };
 }
