f9f0afd8fff3fac065f514a9a4b09f79acd33c10
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-03-27 08:22:28 -0700

    build: upgrade to yarn v2

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/(*-test|test).ts']
+  testMatch: ['**/__tests__/**/(*-test|test).ts'],
+  globals: {
+    'ts-jest': {
+      packageJson: 'package.json'
+    }
+  }
 };
